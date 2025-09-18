4105d8cfeffe2f80d82d44463167d7df3bac2824
Ndivho Confidence Nemutshili <nemutshilindivhoconfidence@gmail.com>
2025-09-18 10:13:21 +0200

    Starting point

---
+++ portfolio/src/app/components/project-ideas/project-ideas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-ideas',
+  imports: [],
+  templateUrl: './project-ideas.component.html',
+  styleUrl: './project-ideas.component.css'
+})
+export class ProjectIdeasComponent {
+
+}
